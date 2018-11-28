e9b140aff9e73f3d24984a0f4a9524c4a2591a35
jonybang <Jonybange@gmail.com>
2018-11-28 12:06:14 +0200

    fix member-payout

---
+++ src/components/PersonalCabinetPage/TariffInfo/TariffInfo.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright ©️ 2018 Galt•Space Society Construction and Terraforming Company 
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka),
+ * [Dima Starodubcev](https://github.com/xhipster), 
+ * [Valery Litvin](https://github.com/litvintech) by 
+ * [Basic Agreement](http://cyb.ai/QmSAWEG5u5aSsUyMNYuX2A2Eaz4kEuoYWUkVBRdmu9qmct:ipfs)).
+ * ​
+ * Copyright ©️ 2018 Galt•Core Blockchain Company 
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) and 
+ * Galt•Space Society Construction and Terraforming Company by 
+ * [Basic Agreement](http://cyb.ai/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS:ipfs)).
+ */
+
+import GaltData from "../../../services/galtData";
+
+export default {
+    name: 'tariff-info',
+    template: require('./TariffInfo.html'),
+    props: ['userWallet'],
+    async mounted() {
+        const member = 
+         GaltData.ethBalance(this.userWallet).then(ethBalance => {
+             this.ethBalance = ethBalance;
+         });
+        this.$coinTokenContract.balanceOf(this.userWallet).then(galtBalance => {
+            this.coinBalance = galtBalance;
+        });
+        this.participant = await this.$cityContract.isMember(this.userWallet);
+    },
+    watch: {
+        
+    },
+    methods: {
+        
+    },
+    data() {
+        return {
+            localeKey: 'personal_cabinet.general_info',
+            ethBalance: null,
+            coinBalance: null,
+            participant: null
+        }
+    }
+}
