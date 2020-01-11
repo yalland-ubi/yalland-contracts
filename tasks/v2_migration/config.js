ddc079f9b2e80d6fd63669016ad035b0e76b976a
Nikita Chebykin <nikita@chebyk.in>
2020-01-11 22:55:31 +0200

    Add v2 migration task 5

--- tasks/v2_migration/config.js
+++ tasks/v2_migration/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     tariffId: '0x58b8674e6776eb4ba8debd64ed4713b6d42b5e2bdfe3022362ea09480536abaf',
     cityAddress: '0x463f8834c322d9e56e2409e562a635dfd5967092',
-    oldTokenAddress: '0x463f8834c322d9e56e2409e562a635dfd5967092'
+    oldTokenAddress: '0x8d4a6cd17d095ef09f460f181546fbec32e11e8b'
 };
