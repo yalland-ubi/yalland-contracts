b114978a172fd5215f10b161ac7fd05f68395f7a
Nikita Chebykin <nikita@chebyk.in>
2020-05-25 16:40:43 +0200

    Add partially working etherscan verification

--- scripts/verifier.js
+++ scripts/verifier.js
@@ -5,13 +5,14 @@ const assert = require('assert');
 const fs = require('fs');
 const Web3 = require('web3');
 const _ = require('lodash');
+const querystring = require('querystring');
 
 axios.interceptors.request.use(request => {
   console.log('Starting Request', request);
   return request;
 });
 
-const network = 'sokol';
+const network = 'kovan';
 const base = 'https://blockscout.com/';
 
 const verifyPost = async (
@@ -67,6 +68,81 @@ const verifyPost = async (
 };
 
 
+const verifyKovanEtherscan = async (
+  name,
+  addressHash,
+  optimization,
+  optimizationRuns,
+  evmVersion,
+  compilerVersion,
+  contractSourceCode,
+  constructorArgs,
+  libs,
+  proxy
+) => {
+  console.log(name, 'verification');
+  assert.ok(name);
+  assert.ok(addressHash);
+  assert.ok(compilerVersion);
+
+  const apiKey = 'GZZTYX65FNXUPIZVABYWY76FDPDHAF3GAA';
+  const base = 'https://api-kovan.etherscan.io';
+
+  const params = {
+    apikey: apiKey,
+    module: 'contract',
+    action: 'verifysourcecode',
+    contractname: name,
+    contractaddress: addressHash,
+    compilerversion: compilerVersion,
+    sourceCode: contractSourceCode,
+    runs: optimizationRuns,
+    evmversion: evmVersion,
+    constructorArguements: constructorArgs ? `0x${constructorArgs}` : '',
+    optimizationUsed: optimization ? 1 : 0
+  };
+
+  if (libs) {
+    for (let i = 0; i < libs.length; i++) {
+      params[`library${i}address`] = libs[i][0];
+      params[`library${i}name`] = libs[i][1];
+    }
+  }
+
+  try {
+    console.log(`Verifying ${name} - ${addressHash}`);
+    let response = await axios({
+      url: '/api',
+      method: 'POST',
+      baseURL: base,
+      data: querystring.stringify(params)
+    });
+    const { data } = response;
+    console.log(data);
+
+    if (data.status == 1) {
+      response = await axios({
+        url: '/api',
+        method: 'GET',
+        baseURL: base,
+        params: {
+          guid: data.result,
+          module: 'contract',
+          action: 'checkverifystatus'
+        }
+      });
+      console.log(response);
+    }
+    // TODO: check explanation
+  } catch (error) {
+    try {
+      console.log(`${name} - ${addressHash} verification error: ${JSON.stringify(error.response.data)}`);
+    } catch (e) {
+      console.log('verification error:', error);
+    }
+  }
+};
+
 const chainSpec = JSON.parse(fs.readFileSync(`deployed/${network}_extended.json`));
 
 async function sleep(timeout) {
@@ -75,6 +151,8 @@ async function sleep(timeout) {
   });
 }
 (async () => {
+  const verify = network === 'sokol' ? verifyPost : verifyKovanEtherscan;
+
   // contract key
   const key = process.argv[2];
   const proxy = process.argv[3];
@@ -86,7 +164,7 @@ async function sleep(timeout) {
 
   if (contract.proxied) {
     if (proxy) {
-      await verifyPost(
+      await verify(
         'AdminUpgradeabilityProxy',
         contract.address,
         chainSpec.compiler.optimizer.enabled,
@@ -97,7 +175,7 @@ async function sleep(timeout) {
         contract.proxyArguments.substring(2)
       )
     } else {
-      await verifyPost(
+      await verify(
         contract.factory,
         contract.implementation,
         chainSpec.compiler.optimizer.enabled,
@@ -109,7 +187,7 @@ async function sleep(timeout) {
       )
     }
   } else {
-    await verifyPost(
+    await verify(
       contract.factory,
       contract.address,
       chainSpec.compiler.optimizer.enabled,
